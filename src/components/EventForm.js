cacd7c01c83e8c6d06ec12b5f4331b5c1b87b06d
y4shiro <y4shironao@gmail.com>
2021-02-02 22:39:58 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,7 +1,13 @@
 import React, { useContext, useState } from 'react';
 
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions';
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS
+} from '../actions';
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -10,7 +16,21 @@ const EventForm = () => {
 
   const addEvent = e => {
     e.preventDefault();
-    dispatch({ type: CREATE_EVENT, title, body });
+
+    // 新規イベントを作成
+    dispatch({
+      type: CREATE_EVENT,
+      title,
+      body
+    });
+
+    // 操作ログを追加
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601()
+    });
+
     setTitle('');
     setBody('');
   };
@@ -18,7 +38,14 @@ const EventForm = () => {
   const deleteAllEvents = e => {
     e.preventDefault();
     const result = window.confirm('全てのイベントを削除してもよろしいですか？');
-    if (result) dispatch({ type: DELETE_ALL_EVENTS });
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENTS });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601()
+      });
+    }
   };
 
   // ボタンの非活性管理
